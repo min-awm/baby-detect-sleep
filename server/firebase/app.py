7ab29e88771551d5253bca3914b5f4c125762aa7
Your Name <you@example.com>
2025-06-06 08:10:16 +0000

    wip

--- server/firebase/app.py
+++ server/firebase/app.py
@@ -1,14 +1,44 @@
+import time
 import firebase_admin
-from firebase_admin import credentials,  db, messaging
+from firebase_admin import credentials, db, messaging
 from helper.path import get_abs_path
 
 cred_path = get_abs_path("baby-sleep-3085d-firebase-adminsdk-fbsvc-40a2ca0158.json", __file__)
 cred = credentials.Certificate(cred_path)
 
-# Realtime database
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://baby-monitor-58b3f-default-rtdb.firebaseio.com/'
 })
+
+# Biến toàn cục theo dõi thời điểm gửi gần nhất
+_last_notification_time = 0
+
+def send_notification_to_token(token, title, body):
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=title,
+            body=body
+        ),
+        token=token
+    )
+
+    response = messaging.send(message)
+    print("Đã gửi thông báo:", response)
+
+def send_notification_to_user(body):
+    global _last_notification_time
+    now = time.time()
+
+    if now - _last_notification_time >= 10:
+        send_notification_to_token(
+            "fHT9_0hLTmOgi8Gpw4PBaG:APA91bHVn-y79PbcVCL7_tJOh9hhB7_Sglrf3JUeIviBTla58G1M1n61Dl6SRJ-KysJF-MRhUTCNPyIfmA-wbPNGuBd9fahU_ET7IFZkgsHSD2lmIZH-iBA",
+            "Baby sleep",
+            body
+        )
+        _last_notification_time = now
+    else:
+        print(f"Bỏ qua gửi: chưa đủ 10 giây. Còn lại: {round(10 - (now - _last_notification_time), 2)}s")
+
 
 # ref = db.reference('/') 
 
@@ -27,20 +57,3 @@ firebase_admin.initialize_app(cred, {
 # user_data = ref.get()
 # print(user_data)
 
-def send_notification_to_token(token, title, body):
-    message = messaging.Message(
-        notification=messaging.Notification(
-            title=title,
-            body=body
-        ),
-        token=token
-    )
-
-    response = messaging.send(message)
-
-def send_notification_to_user(body):
-    send_notification_to_token(
-        "fHT9_0hLTmOgi8Gpw4PBaG:APA91bHVn-y79PbcVCL7_tJOh9hhB7_Sglrf3JUeIviBTla58G1M1n61Dl6SRJ-KysJF-MRhUTCNPyIfmA-wbPNGuBd9fahU_ET7IFZkgsHSD2lmIZH-iBA",
-        "Baby sleep",
-        body
-    )
